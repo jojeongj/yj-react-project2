131d53cad417c60e44de4090b43e40f143ae59f9
seol98 <sjh980218@naver.com>
2023-06-05 16:37:11 +0900

    react

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,10 +1,40 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Button, Input, Radio, RadioGroup, Stack, Text, VStack } from "@chakra-ui/react";
 import Layout from "./layout";
+import { useState } from "react";
 
 export default function Contact() {
-    return <Layout>
-        <Box py="140px">
-            <Text>연락처</Text>
-        </Box>
-    </Layout>
+    const [CSSFontFeatureValuesRule, setValues] = useState({ name: "", email: "", mobile: "" })
+
+    const [name, setName] = useState("")
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
+    return (
+        <Layout>
+            <VStack as="form" py="140px" w="full" alignItems={"flex-start"} px={4} spacing={6}>
+                <Text fontWeight={700} fontSize={20}>
+                    무료상담신청
+                </Text>
+                <VStack w="full" alignItems={"flex-start"}>
+                    <Text fontWeight={600}>이름</Text>
+                    <Input onChange={handleChange} value={name} bg="gray.100" placeholder="이름을 입력해 주세요" />
+                </VStack>
+                <VStack w="full" alignItems={"flex-start"}>
+                    <Text fontWeight={600}>연락처</Text>
+                    <Input bg="gray.100" placeholder="연락처를 입력해 주세요" />
+                </VStack>
+                <VStack w="full" alignItems={"flex-start"}>
+                    <Text fontWeight={600}>email</Text>
+                    <Input bg="gray.100" placeholder="이메일을 입력해 주세요" />
+                </VStack>
+                <RadioGroup>
+                    <Stack direction='row'>
+                        <Radio value='1'>남성</Radio>
+                        <Radio value='2'>여성</Radio>
+                    </Stack>
+                </RadioGroup>
+                <Button colorScheme='blue'>전송하기</Button>
+            </VStack>
+        </Layout>
+    )
 }
--- src/profile.jsx
+++ src/profile.jsx
@@ -1,12 +1,21 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Avatar, Box, HStack, Text, VStack } from "@chakra-ui/react";
 import Layout from "./layout";
 
 export default function Profile() {
     return (
         <Layout>
-            <Box py="140px">
-                <Text>프로필</Text>
-            </Box>
+            <VStack as="form" bg="gray.400" minH="100vh" py="140px" w="full" px={4} spacing={6}>
+                <VStack spacing={6}>
+                    <Avatar src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                        size="xl" color="black" bg="white" name='조정' />
+                    <Text fontSize={20} fontFamily={600}>조정</Text>
+                    <Text fontSize={14}>풀스택 개발자를 꿈꾸는 조정</Text>
+                </VStack>
+                <HStack w="80%">
+                    <Box border="1px" rounded="x1" px="20px" py="1" w="140px" align="center" color="yellow.600">Contact</Box>
+                    <Box border="1px" rounded="x1" px="20px" py="1" w="140px" align="center" color="yellow.600">Follow</Box>
+                </HStack>
+            </VStack>
         </Layout>
     );
 }
